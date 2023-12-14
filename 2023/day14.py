d3e6d756250ab7bcf8c5d9117c37a2af809a8a32
Joao Afonso <joao.afonso@cern.ch>
2023-12-14 11:24:35 +0100

    Finished day 14

--- 2023/day14.py
+++ 2023/day14.py
@@ -14,39 +14,70 @@ test_array = [
     "#OO..#....",
 ]
 
-def calculate_line_load(col: str):
+def calculate_row_load_left(col: str):
     total_load = 0
-    last_anchor_load = len(col)
     for idx, s in enumerate(col):
         if s == "O":
-            total_load += last_anchor_load
-            last_anchor_load -= 1
-        elif s == "#":
-            last_anchor_load = len(col) - idx - 1
+            total_load += len(col) - idx
     return total_load
 
-def calculate_north_load(data: List[str]):
-    columns = zip(*data)
-    return sum(calculate_line_load(col) for col in columns)
+def tilt_left(input: List[str]):
+    return ["#".join(["".join(sorted(seg, reverse=True)) for seg in row.split("#")]) for row in input]
 
-def rotate_anticlock(data: List[str]):
-    return list("".join(z) for z in zip(*data))[::-1]
+def calculate_load_left(input: List[str]):
+    return sum(calculate_row_load_left(row) for row in input)
+
+def rotate_anticlockwise(input: List[str]):
+    return list("".join(z) for z in zip(*input))[::-1]
+
+def rotate_clockwise(input: List[str]):
+    return list("".join(z)[::-1] for z in zip(*input))
+
+def perform_single_cycle(input: List[str]):
+    dish = tilt_left(input) # Tilt north
+    dish = tilt_left(rotate_clockwise(dish)) # Tilt west
+    dish = tilt_left(rotate_clockwise(dish)) # Tilt south
+    dish = tilt_left(rotate_clockwise(dish)) # Tilt east
+    return rotate_clockwise(dish) # Go back to north orientation
+
+def perform_n_cycles(input: List[str], cycles: int):
+
+    # At some point a loop will be found
+    # We need to find it to avoid running all N cycles
+    dish = input
+    previous_dishes = {}
+    for i in range(cycles):
+        tuple_dish = tuple(dish)
+        if tuple_dish in previous_dishes:
+            loop_start = previous_dishes[tuple_dish]
+            loop_size = i - previous_dishes[tuple_dish]
+            break
+        previous_dishes[tuple_dish] = i
+        dish = perform_single_cycle(dish)
+    else:
+        raise RuntimeError("Did not find loop")
+    
+    remaining_cycles = (cycles - loop_start) % loop_size
+    for i in range(remaining_cycles):
+        dish = perform_single_cycle(dish)
+    return dish
 
 if __name__ == "__main__":
 
-    #input_data = aocutils.getDataInput(14)
-    input_data = test_array
+    input_data = aocutils.getDataInput(14)
+    #input_data = test_array
 
     #### Part 1 ####
-    aocutils.printResult(1, calculate_north_load(input_data))
 
-    d1 = rotate_anticlock(input_data)
-    d2 = rotate_anticlock(d1)
-    d3 = rotate_anticlock(d2)
+    # Start by rotating 90 degrees anticlockwise, north becomes left
+    rotated_dish = rotate_anticlockwise(input_data)
+    tilted_dish = tilt_left(rotated_dish)
+    aocutils.printResult(1, calculate_load_left(tilted_dish))
 
-    print("\n".join(d1))
-    print("\n")
-    print("\n".join(d2))
-    print("\n")
-    print("\n".join(d3))
+    #### Part 2 ####
+
+    # Start by rotating 90 degrees anticlockwise, north becomes left
+    rotated_dish = rotate_anticlockwise(input_data)
+    cycled_dish = perform_n_cycles(rotated_dish, 1000000000)    
+    aocutils.printResult(2, calculate_load_left(cycled_dish))
 
