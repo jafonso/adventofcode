d5e1b78206b3baa6d3d85f9797a9dab534089311
Joao Afonso <jlh.afonso@gmail.com>
2023-12-26 10:58:21 +0000

    Finished day 24

--- 2023/day24.py
+++ 2023/day24.py
@@ -1,5 +1,7 @@
 import aocutils
 import numpy as np
+import functools
+import math
 from typing import List, Tuple
 
 test_array = [
@@ -60,10 +62,42 @@ def get_intersection_xy(h1: Tuple[Tuple[int,int,int], Tuple[int,int,int]], h2: T
     if not np.allclose(R1, R2):
         raise RuntimeError(f"Result R1 and R2 not equal, something is wrong:\n{R1}\n{R2}")
     
-    # Return (x, y) and (t1, t2)
-    return np.round(R1, 6).reshape(1,2).tolist()[0], np.round(X, 6).reshape(1,2).tolist()[0]
+    return R1.reshape(1,2).tolist()[0], X.reshape(1,2).tolist()[0]
 
-def calculate_intersections_xy(hailstones: Tuple, min_pos: int, max_pos: int):
+def check_same_speed(hailstones: Tuple[Tuple[int,int,int], Tuple[int,int,int]]):
+    
+    count_vx = dict()
+    count_vy = dict()
+    count_vz = dict()
+
+    for hail in hailstones:
+        (x, y, z), (vx, vy, vz) = hail
+        if vx not in count_vx:
+            count_vx[vx] = set()
+        if vy not in count_vy:
+            count_vy[vy] = set()
+        if vz not in count_vz:
+            count_vz[vz] = set()
+        count_vx[vx].add(hail)
+        count_vy[vy].add(hail)
+        count_vz[vz].add(hail)
+
+    for v, hails in count_vx.items():
+        if len(hails) >= 3:
+            print(f"Same vx {v} at least trice: ", hails)
+    for v, hails in count_vy.items():
+        if len(hails) >= 3:
+            print(f"Same vy {v} at least trice: ", hails)
+    for v, hails in count_vz.items():
+        if len(hails) >= 3:
+            print(f"Same vz {v} at least trice: ", hails)
+
+def normalize_hail(hailstones: Tuple[Tuple[int,int,int], Tuple[int,int,int]]):
+    ref = hailstones[0]
+    (x0, y0, z0), (vx0, vy0, vz0) = ref
+    return ref, [((x-x0, y-y0, z-z0), (vx-vx0, vy-vy0, vz-vz0)) for (x, y, z), (vx, vy, vz) in hailstones]
+
+def calculate_intersections_xy(hailstones: Tuple[Tuple[int,int,int], Tuple[int,int,int]], min_pos: int, max_pos: int):
     count = 0
     for idx, hail_1 in enumerate(hailstones):
         for hail_2 in hailstones[idx+1:]:
@@ -81,6 +115,54 @@ def calculate_intersections_xy(hailstones: Tuple, min_pos: int, max_pos: int):
             count += 1
     return count
 
+def print_position_at(hailsone: Tuple[Tuple[int,int,int], Tuple[int,int,int]], t: float, key: str=""):
+    print(key, np.array(hailsone[0]) + (t * np.array(hailsone[1])))
+
+def find_gcd(arr):
+    return functools.reduce(math.gcd, arr)
+
+def scale_down(v):
+    divisor = find_gcd(v)
+    return v // divisor
+
+def calculate_rock_trajectory(hailstones: List[Tuple[Tuple[int,int,int], Tuple[int,int,int]]]):
+    (hail_0_ref_p, hail_0_ref_v), norm_hailstones = normalize_hail(hailstones)
+
+    norm_hailstones_0 = norm_hailstones[0]
+    norm_hailstones_1 = norm_hailstones[1]
+    norm_hailstones_2 = norm_hailstones[2]
+
+    hail_0_pos, hail_0_vel = norm_hailstones_0
+    hail_1_pos, hail_1_vel = norm_hailstones_1
+    hail_2_pos, hail_2_vel = norm_hailstones_2
+
+    n_plane_1 = scale_down(np.cross(hail_1_pos, hail_1_vel))
+    n_plane_2 = scale_down(np.cross(hail_2_pos, hail_2_vel))
+
+    vel_par = scale_down(np.cross(n_plane_1, n_plane_2))
+
+
+    ########
+
+    temp_rock_vect = (hail_0_pos, vel_par)
+    _, (t_1, _) = get_intersection_xy(norm_hailstones_1, temp_rock_vect)
+    _, (t_2, _) = get_intersection_xy(norm_hailstones_2, temp_rock_vect)
+
+    colision_coords_1 = np.array(norm_hailstones_1[0], dtype=np.float128) + (t_1 * np.array(norm_hailstones_1[1], dtype=np.float128))
+    colision_coords_2 = np.array(norm_hailstones_2[0], dtype=np.float128) + (t_2 * np.array(norm_hailstones_2[1], dtype=np.float128))
+
+    t_diff = t_2 - t_1
+    coord_diff = np.array(colision_coords_2, dtype=np.float128) - np.array(colision_coords_1, dtype=np.float128)
+
+    vel = coord_diff / t_diff
+    pos = np.array(colision_coords_1, dtype=np.float128) - (t_1 * vel)
+
+    # Get back the real values
+    vel_real = vel + np.array(hail_0_ref_v, dtype=np.float128)
+    pos_real = pos + np.array(hail_0_ref_p, dtype=np.float128)
+
+    return pos_real, vel_real
+
 if __name__ == "__main__":
 
     input_data = aocutils.getDataInput(24)
@@ -93,3 +175,6 @@ if __name__ == "__main__":
     aocutils.printResult(1, calculate_intersections_xy(hailstones, 200000000000000, 400000000000000))
 
     #### Part 2 ####
+
+    rock_trajectory = calculate_rock_trajectory(hailstones)
+    aocutils.printResult(2, int(np.sum(rock_trajectory[0])))
